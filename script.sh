c406b79d75804e6e343c65536d883c4c674a06aa
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-04-29 13:12:13 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 echo ${PLUGIN_APPLICATION}
 echo ${PLUGIN_REPO}
-curl ${PLUGIN_BRANCH}
-curl ${PLUGIN_COMMITSHA}
-echo "plugin executed successfully"
+echo ${PLUGIN_BRANCH}
+echo ${PLUGIN_COMMITSHA}
+echo "Variables Passed to CARA Nolio SUCCESSFULLY!"
