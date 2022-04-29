5589006be2b411afdf7bf255ceb375603ba3c523
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-04-29 13:32:58 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-echo ${PLUGIN_APPLICATION}
-echo ${PLUGIN_IMAGENAME}
-echo ${PLUGIN_REPO}
-echo ${PLUGIN_BRANCH}
-echo ${PLUGIN_COMMITSHA}
+echo "Application:" ${PLUGIN_APPLICATION}
+echo "Image Name" ${PLUGIN_IMAGENAME}
+echo "Repo" ${PLUGIN_REPO}
+echo "Branch" ${PLUGIN_BRANCH}
+echo "Commit SHA" ${PLUGIN_COMMITSHA}
 echo "Variables Passed to CARA Nolio SUCCESSFULLY!"
