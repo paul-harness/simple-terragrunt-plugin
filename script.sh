c0c8b1949fb4eef4cc2267e67b5728f4b89d3750
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-05 06:56:58 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
+echo "TF Repo:" ${PLUGIN_REPO}
 echo "AWS Region:" ${PLUGIN_AWSREGION}
 terraform --version
-git clone https://github.com/paul-harness/tf-hello-world.git
+git clone https://github.com/paul-harness/${PLUGIN.REPO}.git
 ls -l
-cd tf-hello-world
+cd ${PLUGIN_REPO}
 ls -l
-
+terraform init
+terraform apply -var="aws_region=${PLUGIN_AWSREGION}" -auto-approve
