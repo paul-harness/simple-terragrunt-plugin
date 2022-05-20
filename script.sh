db42c9ee6c7140c7c95aeafae0ecf0e82510bd6c
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-20 05:27:57 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
-git clone "https://github.com/paul-harness/${PLUGIN_REPO}.git"
-cd ${PLUGIN_REPO}
+git clone ${PLUGIN_REPOURL}
+basename=$(basename ${PLUGIN_REPOURL})
+echo $basename
+filename={basename%.*}
+echo filename
+cd filename
 terraform init
-terraform apply -var="aws_region=${PLUGIN_AWSREGION}" -auto-approve
+terraform apply -var="aws_region=${PLUGIN_AWSREGION}" -var="var1=${PLUGIN_VAR1}" -var="var2=${PLUGIN_VAR2}" -var="var3=${PLUGIN_VAR3}" -auto-approve
