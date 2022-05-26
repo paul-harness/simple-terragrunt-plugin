f84c9be73d8778877e81e3613b5decf895f10b88
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-26 06:27:21 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,4 @@ echo $basename
 filename=${basename%.*}
 echo $filename
 cd $filename
-terraform init
-terraform apply -var="aws_region=${PLUGIN_AWSREGION}" -var="var1=${PLUGIN_VAR1}" -var="var2=${PLUGIN_VAR2}" -var="var3=${PLUGIN_VAR3}" -auto-approve
+terragrunt apply
